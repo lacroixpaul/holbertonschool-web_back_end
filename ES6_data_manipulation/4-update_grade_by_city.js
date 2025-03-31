3e41537651392a7f99969e0b9280bf0a77c4864d
lacroixpaul <paul.lacroix892@outlook.fr>
2025-03-31 11:43:25 +0200

    ES6_data_manipulation/4-update_grade_by_city.js

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,7 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city)
+    .map((student) => {
+      const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
+      return { ...student, grade: newGrade ? newGrade.grade : 'N/A' };
+    });
+}
