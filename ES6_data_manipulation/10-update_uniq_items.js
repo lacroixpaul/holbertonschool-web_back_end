b036e5dd0119c69e6659737cfe59e960682fe745
lacroixpaul <paul.lacroix892@outlook.fr>
2025-03-31 15:57:46 +0200

    ES6_data_manipulation/10-update_uniq_items.js

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(groceryMap) {
+  if (!(groceryMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  groceryMap.forEach((quantity, item) => {
+    if (quantity === 1) {
+      groceryMap.set(item, 100);
+    }
+  });
+  return groceryMap;
+}
