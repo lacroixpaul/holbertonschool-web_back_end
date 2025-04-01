98ffc7d2f825535eaede9bd1c58c9b7843b4b7ec
lacroixpaul <paul.lacroix892@outlook.fr>
2025-04-01 10:22:00 +0200

    ES6_promise/0-promise.js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,4 +1,9 @@
 export default function getResponseFromAPI() {
-  const response = new Promise((resolve) => resolve());
+  const response = new Promise((resolve, reject) => {
+    resolve();
+    setTimeout(() => {
+      reject();
+    }, 2000);
+  });
   return response;
 }
