e328fd9ad254bee43465188c6bc5c27cb891f165
lacroixpaul <paul.lacroix892@outlook.fr>
2025-04-01 15:48:06 +0200

    Fix task 3: Console

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResult, userResult]) => {
-      console.log(photoResult.body, userResult.firstName, userResult.lastName);
+      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
     })
     .catch((error) => {
       console.error('Signup system offline', error);
