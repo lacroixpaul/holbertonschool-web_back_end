c749d4fdf729acaa708120c27d9b4d1c58117d2f
lacroixpaul <paul.lacroix892@outlook.fr>
2025-04-29 12:11:41 +0200

    NoSQL/10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""10-update_topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+    query = {"name": name}
+    new_values = {"$set": {"topics": topics}}
+    mongo_collection.update_one(query, new_values)
