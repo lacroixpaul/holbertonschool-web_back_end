e35f32f9fe9e2f79d92aa9c6f3e20551e8a29fb1
lacroixpaul <paul.lacroix892@outlook.fr>
2025-05-23 11:25:15 +0200

    Node_JS_basic/0-console.js

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -2,4 +2,3 @@ function displayMessage(message) {
 	console.log(message);
 }
 module.exports = displayMessage;
-// The above code defines a function `displayMessage` that takes a message as an argument and logs it to the console.
